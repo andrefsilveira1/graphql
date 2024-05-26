0ff081b3513aade8c3f780a3281faee199f2e645
André Freitas <freitasandre38@gmail.com>
2024-05-26 20:20:10 -0300

    feat: create schema resolvers and instances for course db and solve problems with course queries

--- internal/database/course.go
+++ internal/database/course.go
@@ -7,43 +7,45 @@ import (
 )
 
 type Course struct {
-	db         *sql.DB
-	ID         string
-	Name       string
-	CategoryID string
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+	CategoryID  string
 }
 
 func NewCourse(db *sql.DB) *Course {
 	return &Course{db: db}
 }
 
-func (c *Course) CreateCourse(name, categoryID string) (*Course, error) {
+func (c *Course) CreateCourse(name, description, categoryID string) (*Course, error) {
 	id := uuid.New().String()
-	_, err := c.db.Exec(`INSERT INTO courses (id, name, category_id) VALUS ($1,$2,$3)`, id, name, categoryID)
+	_, err := c.db.Exec(`INSERT INTO courses (id, name, description, category_id) VALUES ($1,$2,$3,$4)`, id, name, description, categoryID)
 	if err != nil {
 		return nil, err
 	}
 
 	return &Course{
-		ID:         id,
-		Name:       name,
-		CategoryID: categoryID,
+		ID:          id,
+		Name:        name,
+		Description: description,
+		CategoryID:  categoryID,
 	}, nil
 }
 
 func (c *Course) ListCourses() ([]Course, error) {
-	rows, err := c.db.Query("SELECT id, name, category_id FROM courses")
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM courses")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 	courses := []Course{}
 	for rows.Next() {
-		var id, name, category_id string
+		var id, name, description, category_id string
 		if err := rows.Scan(&id, &name, &category_id); err != nil {
 			return nil, err
 		}
-		courses = append(courses, Course{ID: id, Name: name, CategoryID: category_id})
+		courses = append(courses, Course{ID: id, Name: name, Description: description, CategoryID: category_id})
 	}
 
 	return courses, nil
